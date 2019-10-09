6fe940e0fd8b5746f195cbe543a4667e1dbb7864
Tanner <tannerbutler15@gmail.com>
2019-10-09 15:15:26 -0500

    updated

---
+++ forms/calculateExtraXP/calculateExtraXP.js
@@ -0,0 +1,22 @@
+/*
+function calcAvgSquare(a,b,c){
+  avgSquare = ((a + b + c)/3) * Math.pow(a,2)
+  return avgSquare
+  }
+function ranMult(a){
+  randomMultiply= a * Math.floor(Math.random() * 10)
+  return randomMultiply
+  }
+let intialQuestion = prompt('Would you like an average or random multiplication?')
+if (intialQuestion.toLowerCase = 'average') {
+    let num1 = Number(prompt('What is your first number?'))
+    let num2 = Number(prompt('What is your second number?'))
+    let num3 = Number(prompt('What is your third number?'))
+    alert(`The answer using ${num1}, ${num2}, and ${num3} is ${calcAvgSquare(num1,num2,num3)}.`)
+} else if (intialQuestion.toLowerCase='random multiplication') {
+    let num4 = Number(prompt('What is your first number?'))
+    alert(`Your number ${num4} multiplied by the random number equaled ${ranMult(num4)}`)
+} else{
+    alert('That is not quite right')
+}
+*/
